4881b600b6fc1881392c920691d2082963dc9449
TuyenVTI <tuyen.laivan@vti.com.vn>
2025-08-12 16:17:19 +0700

    add avatar upload functionality and update profile details

--- profile-service/src/main/java/com/vti/profile/mapper/UserProfileMapper.java
+++ profile-service/src/main/java/com/vti/profile/mapper/UserProfileMapper.java
@@ -1,14 +1,16 @@
 package com.vti.profile.mapper;
 
+import com.vti.profile.dto.request.UpdateProfileRequest;
 import com.vti.profile.dto.response.UserProfileResponse;
 import org.mapstruct.Mapper;
 
 import com.vti.profile.dto.request.ProfileCreationRequest;
 import com.vti.profile.entity.UserProfile;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserProfileMapper {
     UserProfile toUserProfile(ProfileCreationRequest request);
-
-    UserProfileResponse toUserProfileReponse(UserProfile entity);
+    void update(@MappingTarget UserProfile entity, UpdateProfileRequest request);
+    UserProfileResponse toUserProfileResponse(UserProfile entity);
 }
